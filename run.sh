a53b5ca957374b4f398d98cb9038fc0c19ca09da
moonjunyyy <moonjunyyy@naver.com>
2022-08-27 16:52:05 +0900

    test fail

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=moon_L2P
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --time=5:00:00
 #SBATCH -o output_ND.out
 #SBATCH -e errors_ND.err
@@ -15,17 +15,4 @@ python main.py \
     --pool-size 10 \
     --selection-size 5 \
     --prompt-len 5 \
-    --batchsize 128
-
-#SBATCH -o output_ND.out
-#SBATCH -e errors_ND.err
-
-source /home/junyeong/init.sh
-conda activate dualprompt
-python main.py \
-    --data-path "/home/datasets/CIFAR-100/cifar-100-python" \
-    --save-path "saved/l2p/CIL_NoDiversed_10410" \
-    --pool-size 10 \
-    --selection-size 4 \
-    --prompt-len 10 \
     --batchsize 128
--- run2.sh
+++ run2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=moon_L2P
+#SBATCH --job-name=moon_L2P2
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 #SBATCH --time=5:00:00
 #SBATCH -o output_ND2.out
 #SBATCH -e errors_ND2.err
@@ -11,7 +11,7 @@ source /home/junyeong/init.sh
 conda activate dualprompt
 python main.py \
     --data-path "/home/datasets/CIFAR-100/cifar-100-python" \
-    --save-path "saved/l2p/CIL_NoDiversed_10104" \
+    --save-path "saved/l2p/CIL_NoDiversed_10410" \
     --pool-size 10 \
     --selection-size 4 \
     --prompt-len 10 \
